7a946f496fa8d6f6dd01e6693a555d7a6f3471a1
Ex-rael <isragraziolagarcia@gmail.com>
2024-09-10 20:45:31 -0300

    Insert CPU cicles atribute

--- Scheduler/main.cpp
+++ Scheduler/main.cpp
@@ -5,33 +5,38 @@
 
 using namespace std;
   
-enum State {
-    Ready,
-    Running,
-    Blocked,
-    Exit,
+enum class State {
+    READY,
+    RUNNING,
+    BLOCKED,
+    EXIT,
 };
 
 class Process {
     public:
         string name;
-        long  cpu_burst;
+        long  cpu_burst; //
         long  io_time;
         long  total_cpu_time;
+        long  cpu_cicles;
         int   order;
-        int   priority;
+        int   priority; // definido arbitrariamente na criação do processo
         int   credits;
         State state;
-        
-        Process(string name, int priority, int order){
-            this->cpu_burst = 0;
-            this->io_time = 0;
-            this->total_cpu_time = 0;
-            this->order = order;
-            this->priority = priority;
-            this->credits = priority;
-            this->state = State::Ready;
-        };  
+
+    Process(const string& p_name, long p_cpu_burst, long p_io_time, long p_total_cpu_time, long p_cpu_cicles
+            int p_order, int p_priority, int p_credits, State p_state)
+            : name(p_name),
+              cpu_burst(p_cpu_burst),
+              io_time(p_io_time),
+              total_cpu_time(p_total_cpu_time),
+              cpu_cicles(p_cpu_cicles),
+              order(p_order),
+              priority(p_priority),
+              credits(p_credits),
+              state(p_state) {
+
+    }
 };
 
 priority_queue<Process> blocked;
@@ -42,5 +47,5 @@ void exec(Process process) {
 }
 
 int main () {
-    
+    Process p1("Process_1", )
 }
